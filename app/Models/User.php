f1b4f11fd2b918497b6f94b1ed583a6c3b23ba7f
AlexisSM377 <al222111377@gmail.com>
2024-04-24 16:20:41 -0600

    pruebas

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,7 +3,10 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -43,5 +46,27 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function posts(): HasMany
+    {
+        return $this->hasMany(Post::class, 'author_id');
+    }
+
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class, 'author_id');
+    }
+
+    public function isMyself(): bool
+    {
+        return $this->is(auth()->user());
+    }
+
+    protected function firstName(): Attribute
+    {
+        return Attribute::make(
+            get: fn(?string $value) => ucfirst(str($this->name)->explode(' ')->first())
+        );
     }
 }
